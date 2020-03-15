853099704b39ef0e9eeed2b0ab6c116ca689cf1f
Jonny <54645567+jrp369@users.noreply.github.com>
2020-03-14 20:37:49 -0400

    Update feedback.php

--- www/feedback.php
+++ www/feedback.php
@@ -1,10 +1,25 @@
 <?php
-  $message = $_GET['comment'];
-  $email_from = 'endeavortoadventure@gmail.com';
-  $email_subject = "New Feedback";
-  $email_body = "You have received new feedback.\n".
-                            "Here is the comment:\n $message".
-  $to = "endeavortoadventure@gmail.com";
-  $headers = "From: $email_from \r\n";
-  mail($to,$email_subject,$email_body,$headers);
+if(isset($_POST['email'])) {
+    $email_to = "endeavortoadventure@gmail.com";
+    $email_subject = "New Feedback";
+    $comment = $_POST['comment']; // required
+    // $email_from = $_POST['email']; // required
+
+    function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+    }
+
+    $email_message = "Form details below.\n\n";
+    // $email_message .= "Name: ".clean_string($first_name)."\n";
+    // $email_message .= "Email: ".clean_string($email_from)."\n";
+    // $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    // $email_message .= "Location: ".clean_string($location)."\n";
+    //  $email_message .= "Address: ".clean_string($address)."\n";
+
+    // create email headers
+    $headers = 'From: '.$email_from."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    mail($email_to, $email_subject, $email_message, $headers);
 ?>
